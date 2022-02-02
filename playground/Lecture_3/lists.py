3a28bf4fac51a34af1dd130a28387e1eb6d98a5f
Hakan Tosun <mucithakan@hotmail.com>
2022-02-02 21:07:59 +0100

    Lecture 3 - Lists

---
+++ playground/Lecture_3/lists.py
@@ -0,0 +1,40 @@
+# lis = [2,4,6,8]
+# lis[1] = 5
+# lis[0] += 1
+# lis[2] = lis[3] * lis[0]
+# print(lis)
+
+liste = [2,2,3,4,6]
+
+# def yazdiri(lis):
+#     for i in range(0,len(lis)):
+#         print(lis[i])
+
+# yazdiri(liste)
+
+
+# def yazdire(lis):
+#     for e in lis:
+#         print(e)
+
+# yazdire(liste)
+
+
+# def topla(lis):
+#     sum=0
+#     for e in lis:
+#         sum += e
+#     return print(sum)
+
+# topla(liste)
+
+
+def kareAl(lis):
+    for i in range(0,len(lis)):
+        lis[i] = lis[i]*lis[i]
+    return print(lis)
+
+kareAl(liste)
+
+liste.copy()
+liste.reverse()
